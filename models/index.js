7d471cb066020fbb161e0c441b7d04a7354c2a32
Adjoa Hackman <adjoahackman93@gmail.com>
2024-05-13 17:33:38 -0700

    updated import paths and foreign key associations

--- models/index.js
+++ models/index.js
@@ -5,15 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category,{
+  foreignKey: "category_id",
+  onDelete: "CASCADE"
+});
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongstoMany(Tag, {through: 'ProductTag'});
+Product.belongsToMany(Tag, {through: ProductTag, 
+foreignKey: "product_id"});
 // Tags belongToMany Products (through ProductTag)
-Tag.belongstoMany(Product, {through: 'Tag'});
+Tag.belongsToMany(Product, {through: ProductTag,
+foreignKey: "tag_id"});
 // similar to sql syntax - referenced by 
 
 module.exports = {
